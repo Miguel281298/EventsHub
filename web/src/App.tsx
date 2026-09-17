b39c123c3d8161c2a8de4c1a1441c7951e212c08
Miguel Vazquez <miguelornelas54@gmail.com>
2026-09-17 01:16:55 -0600

    feat(parcial_2): add axios rest

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface Activity {
@@ -10,9 +11,9 @@ function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    fetch("https://localhost:5001/api/v1/events")
-      .then(response => response.json())
-      .then(data => setActivities(data));
+    axios.get<Activity[]>("https://localhost:5001/api/v1/events")
+      .then(response => setActivities(response.data));
+    
     return () => {};
   }, []);
     
